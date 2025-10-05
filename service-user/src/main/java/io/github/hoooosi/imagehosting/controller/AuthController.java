00f69da53c555330bb0134b4ecd84eab3c133518
肚子饿饿 <12912039+xryazi1104@user.noreply.gitee.com>
2025-10-05 16:39:41 +0800

    fix(auth): restore missing response fields in user-service auth endpoint

--- service-user/src/main/java/io/github/hoooosi/imagehosting/controller/AuthController.java
+++ service-user/src/main/java/io/github/hoooosi/imagehosting/controller/AuthController.java
@@ -4,7 +4,9 @@ import io.github.hoooosi.imagehosting.annotation.AuthLogged;
 import io.github.hoooosi.imagehosting.dto.BaseRes;
 import io.github.hoooosi.imagehosting.entity.User;
 import io.github.hoooosi.imagehosting.modal.UserToken;
+import io.github.hoooosi.imagehosting.service.UserService;
 import io.github.hoooosi.imagehosting.utils.SessionUtils;
+import io.github.hoooosi.imagehosting.vo.UserInfoVO;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
@@ -19,12 +21,12 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "Auth I/F")
 public class AuthController {
 
+    private final UserService userService;
+
     @Operation(summary = "AUTH")
     @GetMapping("/auth")
-//    @AuthLogged
-    public BaseRes<UserToken> auth() {
-        log.info("isLogged in:{}", SessionUtils.isLogged());
-        log.info("userToken:{}", SessionUtils.getUserToken());
-        return BaseRes.success(SessionUtils.getUserOrThrow());
+    @AuthLogged
+    public BaseRes<UserInfoVO> auth() {
+        return BaseRes.success(new UserInfoVO(userService.getById(SessionUtils.getUserId())));
     }
 }
